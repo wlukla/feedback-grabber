d3ad3a1b17a43e0a3d7af33358e7942fd0fea9b3
wlukla <vladislav.danilochkin@gmail.com>
2020-03-21 15:18:07 +0200

    feat: implement cookie-session with passport

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const keys = require('./config/keys')
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 
 require('./models/User');
 require('./services/passport');
@@ -14,6 +16,16 @@ mongoose.connect(
 );
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.cookieKey],
+  }),
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
