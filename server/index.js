70fe9043067d22921d086eaf287ae4b60f9541af
wlukla <vladislav.danilochkin@gmail.com>
2020-03-21 02:23:55 +0200

    refactor: update project structure with routes and services folders

--- server/index.js
+++ server/index.js
@@ -1,37 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const Keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: Keys.googleClientID,
-      clientSecret: Keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-      console.log('done', done);
-    },
-  ),
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
-);
-
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google'),
-)
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ server/services/passport.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Keys = require('../config/keys');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: Keys.googleClientID,
+      clientSecret: Keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+      console.log('done', done);
+    },
+  ),
+);
